1af62a482a4c9014113c268b091154155aff939c
mairess <trabalho.maires@gmail.com>
2024-08-19 10:07:02 -0300

    style: adjust code indentation

--- src/main/java/org/maires/employee/controller/EmployeeController.java
+++ src/main/java/org/maires/employee/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package org.maires.employee.controller;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import java.util.List;
 import org.maires.employee.controller.dto.EmployeeCreationDto;
 import org.maires.employee.controller.dto.EmployeeDto;
@@ -7,12 +8,17 @@ import org.maires.employee.entity.Employee;
 import org.maires.employee.service.EmployeeService;
 import org.maires.employee.service.exception.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * The type Employee controller.
@@ -22,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
   private final EmployeeService employeeService;
+
 
   /**
    * Instantiates a new Employee controller.
@@ -33,39 +40,96 @@ public class EmployeeController {
     this.employeeService = employeeService;
   }
 
+
   /**
    * Find all employees list.
    *
    * @return the list
    */
   @GetMapping
-  public List<EmployeeDto> findAllEmployees() {
-    return employeeService.findAllEmployees().stream().map(EmployeeDto::fromEntity).toList();
+  public ResponseEntity<List<EmployeeDto>> findAllEmployees() {
+
+    List<EmployeeDto> employees = employeeService
+        .findAll()
+        .stream().map(EmployeeDto::fromEntity)
+        .toList();
+
+    return ResponseEntity.status(HttpStatus.OK).body(employees);
+
   }
 
+
   /**
-   * Find employee by id employee dto.
+   * Find by id employee dto.
    *
    * @param employeeId the employee id
    * @return the employee dto
    * @throws EmployeeNotFoundException the employee not found exception
    */
   @GetMapping("/{employeeId}")
-  public EmployeeDto findEmployeeById(@PathVariable Long employeeId)
-      throws EmployeeNotFoundException {
-    return EmployeeDto.fromEntity(employeeService.findEmployeeById(employeeId));
+  public ResponseEntity<EmployeeDto> findById(
+      @PathVariable Long employeeId
+  ) throws EmployeeNotFoundException {
+
+    EmployeeDto employee = EmployeeDto.fromEntity(employeeService.findById(employeeId));
+
+    return ResponseEntity.status(HttpStatus.OK).body(employee);
+
   }
 
+
   /**
-   * Create employee employee dto.
+   * Create employee dto.
    *
    * @param employeeCreationDto the employee creation dto
    * @return the employee dto
    */
   @PostMapping
-  public EmployeeDto createEmployee(@RequestBody EmployeeCreationDto employeeCreationDto) {
-    Employee newEmployee = employeeService.createEmployee(employeeCreationDto.toEntity());
-    return EmployeeDto.fromEntity(newEmployee);
+  public ResponseEntity<EmployeeDto> create(@RequestBody EmployeeCreationDto employeeCreationDto) {
+
+    Employee newEmployee = employeeService.create(employeeCreationDto.toEntity());
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(EmployeeDto.fromEntity(newEmployee));
+
+  }
+
+
+  /**
+   * Update employee dto.
+   *
+   * @param employeeId          the employee id
+   * @param employeeCreationDto the employee creation dto
+   * @return the employee dto
+   * @throws JsonMappingException      the json mapping exception
+   * @throws EmployeeNotFoundException the employee not found exception
+   */
+  @PutMapping("/{employeeId}")
+  public ResponseEntity<EmployeeDto> update(
+      @PathVariable Long employeeId,
+      @RequestBody EmployeeCreationDto employeeCreationDto
+  ) throws JsonMappingException, EmployeeNotFoundException {
+
+    Employee employeeUpdated = employeeService.update(employeeId, employeeCreationDto);
+
+    return ResponseEntity.status(HttpStatus.OK).body(EmployeeDto.fromEntity(employeeUpdated));
+
+  }
+
+  /**
+   * Delete by id.
+   *
+   * @param employeeId the employee id
+   * @throws EmployeeNotFoundException the employee not found exception
+   */
+  @DeleteMapping("/{employeeId}")
+  public ResponseEntity<Void> deleteById(
+      @PathVariable Long employeeId
+  ) throws EmployeeNotFoundException {
+
+    employeeService.deleteById(employeeId);
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+
   }
 
 }
--- src/main/java/org/maires/employee/service/EmployeeService.java
+++ src/main/java/org/maires/employee/service/EmployeeService.java
@@ -1,11 +1,16 @@
 package org.maires.employee.service;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import java.util.List;
+import org.maires.employee.controller.dto.EmployeeCreationDto;
 import org.maires.employee.entity.Employee;
 import org.maires.employee.repository.EmployeeRepository;
 import org.maires.employee.service.exception.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 /**
  * The type Employee service.
@@ -14,46 +19,98 @@ import org.springframework.stereotype.Service;
 public class EmployeeService {
 
   private final EmployeeRepository employeeRepository;
+  private final ObjectMapper objectMapper;
+
 
   /**
    * Instantiates a new Employee service.
    *
    * @param employeeRepository the employee repository
+   * @param objectMapper       the object mapper
    */
   @Autowired
-  public EmployeeService(EmployeeRepository employeeRepository) {
+  public EmployeeService(EmployeeRepository employeeRepository, ObjectMapper objectMapper) {
     this.employeeRepository = employeeRepository;
+    this.objectMapper = objectMapper;
   }
+
 
   /**
    * Find all list.
    *
    * @return the list
    */
-  public List<Employee> findAllEmployees() {
+  public List<Employee> findAll() {
+
     return employeeRepository.findAll();
+
   }
 
+
   /**
-   * Find employee by id employee.
+   * Find by id employee.
    *
    * @param employeeId the employee id
    * @return the employee
    * @throws EmployeeNotFoundException the employee not found exception
    */
-  public Employee findEmployeeById(Long employeeId) throws EmployeeNotFoundException {
+  public Employee findById(Long employeeId) throws EmployeeNotFoundException {
+
     return employeeRepository.findById(employeeId).orElseThrow(
+
         () -> new EmployeeNotFoundException(employeeId.toString())
+
     );
+
   }
 
+
   /**
-   * Create employee employee.
+   * Create employee.
    *
    * @param employee the employee
    * @return the employee
    */
-  public Employee createEmployee(Employee employee) {
+  public Employee create(Employee employee) {
+
     return employeeRepository.save(employee);
+
+  }
+
+
+  /**
+   * Update employee.
+   *
+   * @param employeeId          the employee id
+   * @param employeeCreationDto the employee creation dto
+   * @return the employee
+   * @throws EmployeeNotFoundException the employee not found exception
+   * @throws JsonMappingException      the json mapping exception
+   */
+  @Transactional
+  public Employee update(
+      Long employeeId,
+      EmployeeCreationDto employeeCreationDto
+  ) throws EmployeeNotFoundException, JsonMappingException {
+
+    Employee employeeToUpdate = findById(employeeId);
+
+    objectMapper.updateValue(employeeToUpdate, employeeCreationDto);
+
+    return employeeRepository.save(employeeToUpdate);
+
+  }
+
+  /**
+   * Delete by id.
+   *
+   * @param employeeId the employee id
+   * @throws EmployeeNotFoundException the employee not found exception
+   */
+  public void deleteById(Long employeeId) throws EmployeeNotFoundException {
+
+    Employee employee = findById(employeeId);
+
+    employeeRepository.delete(employee);
   }
 }
