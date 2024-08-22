9adf5bf76f8a0c69ef3c88f6e1472279107725a6
mairess <trabalho.maires@gmail.com>
2024-08-22 18:12:33 -0300

    config: initial config password-reset service

---
+++ ms-reset-password/src/test/java/org/maires/password/MsResetPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maires.password;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsResetPasswordApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
