1dd1e20bc95e48ae9fca962f0430531e0baaa5c6
mairess <trabalho.maires@gmail.com>
2024-08-22 18:14:40 -0300

    feat: user entity, user repository and password dtos

---
+++ ms-reset-password/src/main/java/org/maires/password/controller/dto/PasswordResetDto.java
@@ -0,0 +1,8 @@
+package org.maires.password.controller.dto;
+
+/**
+ * The type Password reset dto.
+ */
+public record PasswordResetDto(String newPassword) {
+
+}
---
+++ ms-reset-password/src/main/java/org/maires/password/entity/User.java
@@ -0,0 +1,188 @@
+package org.maires.password.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.List;
+import org.maires.password.security.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * The type User.
+ */
+@Entity(name = "users")
+public class User implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String fullName;
+
+  @Column(unique = true)
+  private String username;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  /**
+   * Instantiates a new User.
+   */
+  public User() {
+  }
+
+
+  /**
+   * Instantiates a new User.
+   *
+   * @param fullName the name
+   * @param username the username
+   * @param email    the email
+   * @param password the password
+   * @param role     the role
+   */
+  public User(String fullName, String username, String email, String password, Role role) {
+    this.fullName = fullName;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
+  public String getFullName() {
+    return fullName;
+  }
+
+  /**
+   * Sets name.
+   *
+   * @param fullName the name
+   */
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  /**
+   * Sets username.
+   *
+   * @param username the username
+   */
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
+
+  /**
+   * Gets email.
+   *
+   * @return the email
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Sets email.
+   *
+   * @param email the email
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.getName()));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  /**
+   * Sets password.
+   *
+   * @param password the password
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  /**
+   * Gets role.
+   *
+   * @return the role
+   */
+  public Role getRole() {
+    return role;
+  }
+
+  /**
+   * Sets role.
+   *
+   * @param role the role
+   */
+  public void setRole(Role role) {
+    this.role = role;
+  }
+}
