2ff47fa6b2a97757f0ea51550fd4b002174deb44
mairess <trabalho.maires@gmail.com>
2024-08-25 07:23:59 -0300

    test(PasswordResetIntegrationTest): remove MockBean, add kafka container and test not found exceptions

--- backend/src/test/java/org/maires/employee/integration/PasswordResetIntegrationTest.java
+++ backend/src/test/java/org/maires/employee/integration/PasswordResetIntegrationTest.java
@@ -13,12 +13,10 @@ import org.maires.employee.controller.dto.PasswordResetRequestDto;
 import org.maires.employee.entity.User;
 import org.maires.employee.repository.UserRepository;
 import org.maires.employee.security.Role;
-import org.maires.employee.service.PasswordResetService;
 import org.maires.employee.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -26,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,11 +36,11 @@ public class PasswordResetIntegrationTest {
   public static PostgreSQLContainer POSTGRES_CONTAINER = new PostgreSQLContainer("postgres")
       .withDatabaseName("employee-db");
 
+  @Container
+  public static KafkaContainer KAFKA_CONTAINER = new KafkaContainer("apache/kafka:3.8.0");
+
   @Autowired
   UserRepository userRepository;
-
-  @MockBean
-  PasswordResetService passwordResetService;
 
   @Autowired
   MockMvc mockMvc;
@@ -56,6 +55,11 @@ public class PasswordResetIntegrationTest {
     registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
   }
 
+  @DynamicPropertySource
+  public static void overrideKafkaProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.kafka.bootstrap-servers", KAFKA_CONTAINER::getBootstrapServers);
+  }
+
   @BeforeEach
   public void cleanUp() {
     userRepository.deleteAll();
@@ -66,8 +70,8 @@ public class PasswordResetIntegrationTest {
   }
 
   @Test
-  @DisplayName("Request password reset")
-  public void testPasswordRequest() throws Exception {
+  @DisplayName("Update password")
+  public void testUpdatePassword() throws Exception {
 
     PasswordResetDto passwordResetDto = new PasswordResetDto("segredo123");
 
@@ -87,8 +91,30 @@ public class PasswordResetIntegrationTest {
   }
 
   @Test
-  @DisplayName("Reset password")
-  public void testResetPassword() throws Exception {
+  @DisplayName("User not found on update password")
+  public void testUserNotFoundExceptionOnUpdatePassword() throws Exception {
+
+    PasswordResetDto passwordResetDto = new PasswordResetDto("segredo123");
+
+    String token = tokenService.generateToken("issoNonEcziste@example.com");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String newPassword = objectMapper.writeValueAsString(passwordResetDto);
+
+    String passwordUrl = "/password/reset?token=%s".formatted(token);
+
+    mockMvc.perform(post(passwordUrl)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(newPassword))
+        .andExpect(status().isNotFound())
+        .andExpect(
+            jsonPath("$.message").value("User not found with id issoNonEcziste@example.com!"));
+
+  }
+
+  @Test
+  @DisplayName("Request password reset")
+  public void testPasswordRequest() throws Exception {
 
     PasswordResetRequestDto passwordResetRequestDto = new PasswordResetRequestDto(
         userAdmin.getEmail());
@@ -103,6 +129,27 @@ public class PasswordResetIntegrationTest {
             .content(newPasswordRequest))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.message").value("Password reset link sent to your email!"));
+
+  }
+
+  @Test
+  @DisplayName("User not found on request password reset")
+  public void testUserNotFoundExceptionOnPasswordRequest() throws Exception {
+
+    PasswordResetRequestDto passwordResetRequestDto = new PasswordResetRequestDto(
+        "issoNonEcziste@example.com");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String newPasswordRequest = objectMapper.writeValueAsString(passwordResetRequestDto);
+
+    String passwordUrl = "/password/reset-request";
+
+    mockMvc.perform(post(passwordUrl)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(newPasswordRequest))
+        .andExpect(status().isNotFound())
+        .andExpect(
+            jsonPath("$.message").value("User not found with id issoNonEcziste@example.com!"));
 
   }
 
