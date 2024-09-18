5e301a849655cedcd373f1c7bf8fcdef8b2a3c5e
mairess <trabalho.maires@gmail.com>
2024-09-18 11:06:53 -0300

    fix: useWindowWidth ReferenceError: window is not defined

--- frontend/src/hooks/useWindowWidth.tsx
+++ frontend/src/hooks/useWindowWidth.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 
 function useWindowWidth() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isClient = typeof window === 'object';
+
+  const [windowWidth, setWindowWidth] = useState(
+    isClient ? window.innerWidth : undefined,
+  );
 
   useEffect(() => {
+    if (!isClient) return;
+
     const handleSize = () => setWindowWidth(window.innerWidth);
 
     window.addEventListener('resize', handleSize);
+
     return () => window.removeEventListener('resize', handleSize);
-  }, []);
+  }, [isClient]);
   return windowWidth;
 }
 
