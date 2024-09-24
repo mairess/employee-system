4c4fa48a19aa307603b24fdce8adb8ee56bb1105
mairess <trabalho.maires@gmail.com>
2024-09-24 09:53:40 -0300

    feat: import getColSpan on RowDetail

--- frontend/src/components/RowDetail.tsx
+++ frontend/src/components/RowDetail.tsx
@@ -1,4 +1,5 @@
 import useWindowWidth from '../hooks/useWindowWidth';
+import getColSpan from '../utils/handleColSpan';
 
 /* eslint-disable max-len */
 type RowDetailProps = {
@@ -10,19 +11,12 @@ type RowDetailProps = {
 function RowDetail({ employeeData, head, breakpoint }:RowDetailProps) {
   const windowWidth = useWindowWidth();
 
-  const getColSpan = () => {
-    if (windowWidth >= 1024) return 6;
-    if (windowWidth >= 768) return 5;
-    if (windowWidth >= 640) return 4;
-    return 3;
-  };
-
   return (
     <tr className="text-black-neutral">
 
       <td
         className={ `px-spacing-regular-20 py-spacing-little-04 ${breakpoint}` }
-        colSpan={ getColSpan() }
+        colSpan={ getColSpan(windowWidth) }
       >
         <div className="flex justify-between border-b border-dashed border-b-gray-neutral-10">
           <h2 className="text-h2 text-black-neutral">{head}</h2>
