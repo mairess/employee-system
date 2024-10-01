2152314944a7c0ef2bb9f08b3b58af42bac7a1ed
mairess <trabalho.maires@gmail.com>
2024-10-01 16:23:21 -0300

    feat: add conditional statement to render admin resources

--- frontend/src/utils/handleColSpan.ts
+++ frontend/src/utils/handleColSpan.ts
@@ -1,5 +1,5 @@
-const getColSpan = (windowWidth: number) => {
-  if (windowWidth >= 1024) return 6;
+const getColSpan = (windowWidth: number, isAdmin: boolean) => {
+  if (windowWidth >= 1024) return isAdmin ? 6 : 5;
   if (windowWidth >= 768) return 5;
   if (windowWidth >= 640) return 4;
   return 3;
